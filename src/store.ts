32f87ce76b4973235f31a0a43ce1558916fcc6be
eugene <veretennikov.evgeniy@outlook.com>
2024-11-10 14:20:19 +0400

    feat
    added save and load task from localstorage

--- src/store.ts
+++ src/store.ts
@@ -1,8 +1,35 @@
-import { configureStore } from '@reduxjs/toolkit'
-import todoReducer from './todoSlice';
+import { combineReducers, configureStore } from "@reduxjs/toolkit";
+import todoReducer, { Todo } from "./todoSlice";
 
-export default configureStore({
-  reducer: {
-    todos: todoReducer,
-  },
-})
+const saveToLocalStorage = (state: { todos: Todo[] }) => {
+  try {
+    const serializedState = JSON.stringify(state);
+    localStorage.setItem("store", serializedState);
+  } catch (error) {
+    console.error(error);
+  }
+};
+
+const loadFromLocalStorage = () => {
+  try {
+    const serializedState = localStorage.getItem("store");
+    if (serializedState === null) return undefined;
+    return JSON.parse(serializedState);
+  } catch (error) {
+    console.error(error); 
+    return undefined;
+  }
+};
+
+const rootReducer = combineReducers({
+  todos: todoReducer,
+});
+
+const store = configureStore({
+  reducer: rootReducer,
+  preloadedState: loadFromLocalStorage(), // Load state from localStorage
+});
+
+store.subscribe(() => saveToLocalStorage(store.getState())); // Save state to localStorage on every change
+
+export default store;
